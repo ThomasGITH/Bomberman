c4fd929315dad3389608669dcf2fb00cf044eaa8
Thomas Linssen <31830553+ThomasGITH@users.noreply.github.com>
2019-09-03 15:54:07 +0200

    Add files via upload

--- Game/Entities.js
+++ Game/Entities.js
@@ -1,19 +1,23 @@
-class Obstacle {
-    constructor(x,y,c){
+class Player {
+    constructor(x,y){
         this.x=x;
         this.y=y;
 
-        this.scaleX = 100;
-        this.scaleY = 100;
+        this.scaleX = 53;
+        this.scaleY = 53;
 
-        this.context = c;
         this.texture = new Image();
-        this.texture.src = 'images/Avatar.png';
+        this.texture.src = 'images/6player.png';
+
+        this.timer = 0;
     }
+
 
     update(){
         var xPrevious = this.x;
         var yPrevious = this.y;
+
+        this.timer--;
 
         if(Input['w'])
         {
@@ -32,9 +36,17 @@ class Obstacle {
             this.x+=3;
         }
 
-        if(hasCollided(this, Bob))
+        if(Input[' ']&& this.timer <= 0)
         {
-            console.log(("collision"));
+            var bombX = (Math.round(this.x / 75) * 75) + 37.5 - 17.5;
+            var bombY = (Math.round(this.y / 75) * 75) + 37.5 - 22.5;
+
+            entityList.push(new Bomb(bombX, bombY));
+            this.timer = 300;
+        }
+
+        if(hasCollided(this, Rock) || hasCollided(this, Wood) || this.x <= 0 || this.x + this.scaleX >= canvas.width || this.y <= 0 || this.y + this.scaleY >= canvas.height)
+        {
             this.y = yPrevious;
             this.x = xPrevious;
         }
@@ -43,25 +55,117 @@ class Obstacle {
 
 }
 
-class Bob {
+class Player2 {
     constructor(x,y){
         this.x=x;
         this.y=y;
 
-        this.scaleX = 230;
-        this.scaleY = 230;
+        this.scaleX = 53;
+        this.scaleY = 53;
 
         this.texture = new Image();
-        this.texture.src = 'images/icon2.png';
+        this.texture.src = 'images/5player2.png';
+
+        this.timer = 0;
+    }
+
+    update(){
+        var xPrevious = this.x;
+        var yPrevious = this.y;
+        this.timer--;
+
+        if(Input['ArrowUp'])
+        {
+            this.y-=3;
+        }
+        if(Input['ArrowLeft'])
+        {
+            this.x-=3;
+        }
+        if(Input['ArrowDown'])
+        {
+            this.y+=3;
+        }
+        if(Input['ArrowRight'])
+        {
+            this.x+=3;
+        }
+
+        if(Input['Enter']&& this.timer <= 0)
+        {
+            var bombX = (Math.round(this.x / 75) * 75) + 37.5 - 17.5;
+            var bombY = (Math.round(this.y / 75) * 75) + 37.5 - 22.5;
+            console.log(bombX);
+            console.log(bombY);
+
+            entityList.push(new Bomb(bombX, bombY));
+            this.timer = 300;
+        }
+
+        if(hasCollided(this, Rock) || hasCollided(this, Wood) || this.x <= 0 || this.x + this.scaleX >= canvas.width || this.y <= 0 || this.y + this.scaleY >= canvas.height)
+        {
+            this.y = yPrevious;
+            this.x = xPrevious;
+        }
+
+    }
+
+}
+
+class Rock {
+    constructor(x,y){
+        this.x=x;
+        this.y=y;
+
+        this.scaleX = 75;
+        this.scaleY = 75;
+
+        this.texture = new Image();
+        this.texture.src = 'images/4Stone.jpg';
 
     }
 
     update(){
-        if(Input[' '])
-        {
-            this.scaleX++;
-            this.scaleY++;
-        }
+
+
+    }
+
+}
+
+class Wood {
+    constructor(x,y){
+        this.x=x;
+        this.y=y;
+
+        this.scaleX = 75;
+        this.scaleY = 75;
+
+        this.texture = new Image();
+        this.texture.src = 'images/3Wood.png';
+
+    }
+
+    update(){
+
+    }
+
+}
+
+class Bomb {
+    constructor(x,y){
+        this.x=x;
+        this.y=y;
+
+        this.scaleX = 35;
+        this.scaleY = 45;
+
+        this.texture = new Image();
+        this.texture.src = 'images/9bomb.png';
+
+    }
+
+    update(){
+
 
     }
 
--- Game/script.js
+++ Game/script.js
@@ -1,33 +1,38 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-canvas.width = window.innerWidth-800;
-canvas.height = window.innerHeight-100;
+canvas.width = 1125;
+canvas.height = 825;
+
+var tile = 75 * 75;
+37.5
 
 var entityList = [];
 var Input={};
 
+var background;
+
 function start() {
-    entityList.push(new Obstacle(500,150));
-    entityList.push(new Bob(90,70));
-    entityList.push(new Bob(700,400));
+     background = new Image();
+    background.src = 'images/1Grass.jpg';
 
-    Input = {"w":false,"a":false,"s":false,"d":false,"up":false,"left":false,"down":false,"right":false, " ":false}
+    entityList.push(new Player(10,10));
+    entityList.push(new Player2(1062,762));
 
-    MainLoop();
+    Input = {"w":false,"a":false,"s":false,"d":false,"ArrowUp":false,"ArrowLeft":false,"ArrowDown":false,"ArrowRight":false, " ":false, "Enter":false}
 }
 
 function MainLoop() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-
     requestAnimationFrame(MainLoop);
+    context.drawImage(background,0,0,1125,825);
 
     for(i=0;i<entityList.length;i++){
         entityList[i].update();
         context.drawImage(entityList[i].texture,entityList[i].x,entityList[i].y, entityList[i].scaleX, entityList[i].scaleY);
     }
+
 }
 
-start();
 window.addEventListener( "keydown", keyPressedDOWN, true);
 window.addEventListener( "keyup", keyPressedUP, true);
 
@@ -59,4 +64,33 @@ function hasCollided(objA, clsstype)
     return collision;
 }
 
+Level1=[
+    '002222222222200',
+    '012121212121210',
+    '222222222222222',
+    '212121212121212',
+    '222222222222222',
+    '212121212121212',
+    '222222222222222',
+    '212121212121212',
+    '222222222222222',
+    '012121212121210',
+    '002222222222200',
 
+
+];
+
+function create() {
+    for (let i = 0; i < Level1.length; i++) {
+        for (let j = 0; j < Level1[i].length; j++) {
+            switch (Level1[i].charAt(j)) {
+                case '1':entityList.push(new Rock(j*75,i*75));break;
+                case '2':entityList.push(new Wood(j*75,i*75));break;
+
+                default: break;}
+        }}}
+
+
+start();
+create();
+MainLoop();
